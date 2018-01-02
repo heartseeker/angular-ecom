66403cf22e4ef59bcaff9843b9264fef26a214bd
Amistad Alex (ERNI) <amal@erni.ph>
2018-01-02 16:07:20 +0800

    added auth with google

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent implements OnInit  {
 
-  constructor() { }
+  user: firebase.User;
+
+  constructor(private _afAuth: AngularFireAuth) {}
 
   ngOnInit() {
+    this._afAuth.authState.subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this._afAuth.auth.signOut();
   }
 
 }
