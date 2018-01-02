2001203bddfd475d0d491749b3b97ac775872975
Amistad Alex (ERNI) <amal@erni.ph>
2018-01-02 15:28:43 +0800

    add firebase and add template styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyARoUUdbvTlrXBDM3fEy64lskJEGYi18T0',
+    authDomain: 'angular-ecom.firebaseapp.com',
+    databaseURL: 'https://angular-ecom.firebaseio.com',
+    projectId: 'angular-ecom',
+    storageBucket: 'angular-ecom.appspot.com',
+    messagingSenderId: '273383936433'
+  }
 };
